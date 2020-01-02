c579a0587d325fea75b54b9d26bcf9cc089ab4dc
Misha Melnyk <misha@melnyk.org>
2020-01-02 00:17:41 -0500

    Removed Dummy Lobbies

--- TicTacToeServer/tictactoeServer.go
+++ TicTacToeServer/tictactoeServer.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"strconv"
 
-	"./game"
 	"./lobby"
 	"./logging"
 	"./naming"
@@ -32,19 +30,15 @@ func main() {
 	go naming.HandleNames()
 	go lobby.HandleLobbies()
 
-	lobbyChan := make(chan string)
-	for i := 1; i < 5; i++ {
-		gl := lobby.NewGameLobby("Lobby"+strconv.Itoa(i), i, i*i+6, i%2+1, i+2)
-		lobby.LobbyChannel <- lobbyChan
-		lobbyChan <- "n" + gl.EncodeMin()
-		s := <-lobbyChan
-		gl2 := <-lobby.GameChan
-		go game.HandleGame(gl2)
-		s += "3"
-	}
-
-	// for i := 0; i < len(lobbies); i++ {
-	// 	fmt.Println(lobbies[i].encode())
+	// lobbyChan := make(chan string)
+	// for i := 1; i < 5; i++ {
+	// 	gl := lobby.NewGameLobby("Lobby"+strconv.Itoa(i), i, i*i+6, i%2+1, i+2)
+	// 	lobby.LobbyChannel <- lobbyChan
+	// 	lobbyChan <- "n" + gl.EncodeMin()
+	// 	s := <-lobbyChan
+	// 	gl2 := <-lobby.GameChan
+	// 	go game.HandleGame(gl2)
+	// 	s += "3"
 	// }
 
 	listener, err := net.Listen("tcp4", PORT) //Starts listener on the chosen port
